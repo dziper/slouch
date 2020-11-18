4b05bfe5fd687020fb0a25756bd7d320f157e3f8
aaaaaaaakash <aakash.davasam@gmail.com>
2020-11-17 17:41:10 -0800

    tried incorporating the rectangle cropping

--- slouchDetector.py
+++ slouchDetector.py
@@ -14,9 +14,6 @@ SLOUCH_TIMER = 5
 MAX_COUNTOUR_SIZE = 800
 
 ESC = 27
-P_KEY = 112
-C_KEY = 99
-S_KEY = 115
 
 centerAngle = 54
 togglePause = False
@@ -47,6 +44,28 @@ def getMousePos(event, x, y, flags, param):
     if event == cv.EVENT_MOUSEMOVE:
         mouseX = x
         mouseY = y
+
+refPt = []
+cropping = False
+
+def click_n_crop(event, x, y, flags, param):
+	# grab references to the global variables
+	global refPt, cropping
+	# if the left mouse button was clicked, record the starting
+	# (x, y) coordinates and indicate that cropping is being
+	# performed
+	if event == cv.EVENT_LBUTTONDOWN:
+		refPt = [(x, y)]
+		cropping = True
+	# check to see if the left mouse button was released
+	elif event == cv.EVENT_LBUTTONUP:
+		# record the ending (x, y) coordinates and indicate that
+		# the cropping operation is finished
+		refPt.append((x, y))
+		cropping = False
+		# draw a rectangle around the region of interest
+		cv.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+		cv.imshow("image", image)
 
 
 cv.setMouseCallback(name, getMousePos)
@@ -172,17 +191,17 @@ while key != ESC:
     cv.imshow(name, np.hstack([frame, cv.cvtColor(threshed, cv.COLOR_GRAY2BGR)]))
     # cv.imshow(name, frame)
 
-    if key == C_KEY:
+    if key == ord('c'):
         # if user presses C
         print("calibrated current angle")
         centerAngle = angleBetween
 
-    if key == P_KEY:
+    if key == ord('p'):
         # if user presses P
         print("play/pause")
         togglePause = not togglePause
 
-    if key == S_KEY:
+    if key == ord('s'):
         ds = str(currTime)
         cutString = ds[0:10] + "-" + ds[11:19]
         dateString = cutString.replace(":", "-")
@@ -194,7 +213,31 @@ while key != ESC:
         cv.imwrite(overlayFile, frame)
         print("Saving Overlay Frame in " + overlayFile)
 
+    if key == ord('x'):
+        togglePause = not togglePause
+        cv.imwrite('image.jpg', frame)
+        image = cv.imread('image.jpg')
+        clone = image.copy()
+        cv.namedWindow("image")
+        cv.setMouseCallback("image", click_n_crop)
 
+        # keep looping until the 'q' key is pressed
+        while True:
+        	# display the image and wait for a keypress
+        	cv.imshow("image", image)
+        	key = cv.waitKey(1) & 0xFF
+        	# if the 'r' key is pressed, reset the cropping region
+        	if key == ord("r"):
+        		image = clone.copy()
+        	# if the 'c' key is pressed, break from the loop
+        	elif key == ord("c"):
+        		break
+        # if there are two reference points, then crop the region of interest
+        # from the image and display it
+        if len(refPt) == 2:
+        	roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+        	cv.imshow("ROI", roi)
+        	cv.waitKey(0)
 
     if startTime != None:
         if (currTime-startTime).total_seconds() > SLOUCH_TIMER:
