3a72d4fb181846effc9092e79175f1884055b1a4
dziper <dziper@me.com>
2020-11-19 11:39:11 -0800

    weird search thing doesn't really work

--- lineDetection.py
+++ lineDetection.py
@@ -9,6 +9,7 @@ split1 = 20
 split2 = 70
 CORR_COEFF_THRESH = 0.95
 
+# set up data
 x = np.arange(length)
 y = np.array([])
 
@@ -19,13 +20,12 @@ for i in range(split2 - split1):
 for i in range(length - split2):
     y = np.append(y, y[split2 - 1] + i*0.5)
 
+# add noise
 for i in range(length):
     y[i] = y[i] + random.random() * random.randint(-5,5)
 
 
-print(datetime.datetime.now())
 slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
-print(datetime.datetime.now())
 corrCoeff = r_value ** 2
 
 print(slope)
@@ -47,23 +47,36 @@ def tryLine(x,y,low,high):
     return corrCoeff
 
 
-low = 0.25
-high = 0.75
+low = 0
+high = 1
 
 corrStepThresh = 0.01
 corr = tryLine(x,y,low,high)
 prevCorr = corr
 iters = 0
 max_iters = 10
+low = 0.5
 
 while corr < CORR_COEFF_THRESH and iters < max_iters:
+    if corr > prevCorr - corrStepThresh:
+        low = low/2
+    else:
+        low = low+low/2
 
     corr = tryLine(x,y,low,high)
-
-    if corr > prevCorr - corrStepThresh:
-
 
     prevCorr = corr
     iters += 1
 
-tryLine(x,y,0.25,0.75)
+CORR_COEFF_THRESH = 0.98
+while corr < CORR_COEFF_THRESH and iters < max_iters:
+    corr = tryLine(x,y,low,high)
+
+    if corr > prevCorr - corrStepThresh:
+        high = high - high/2
+    else:
+        high = high/2
+    if high == low:
+        print("bad")
+    prevCorr = corr
+    iters += 1
