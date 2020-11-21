9e8d36af0fdd6f915cdfa5a02dcc98742c7f8714
dziper <dziper@me.com>
2020-11-20 17:44:36 -0800

    works a little better

--- lineDetection.py
+++ lineDetection.py
@@ -5,7 +5,7 @@ import random
 import datetime
 
 length = 100
-split1 = 20
+split1 = 10
 split2 = 70
 CORR_COEFF_THRESH = 0.95
 
@@ -14,11 +14,11 @@ x = np.arange(length)
 y = np.array([])
 
 for i in range(split1):
-    y = np.append(y, i*4)
+    y = np.append(y, i*5)
 for i in range(split2 - split1):
     y = np.append(y, y[split1 - 1] + i*1.2)
 for i in range(length - split2):
-    y = np.append(y, y[split2 - 1] + i*0.2)
+    y = np.append(y, y[split2 - 1] + i*0.1)
 
 # add noise
 for i in range(length):
@@ -54,20 +54,21 @@ def tryLine(x,y,low,high, plot = False):
 
 
 
-low = 0
-high = 1
+low = 0.35
+high = 0.65
 
 corrStepThresh = -0.01
 corr = tryLine(x,y,low,high)
-prevScore = corr
+prevScore = 0
 iters = 0
 max_iters = 10
-low = 0
-change = 0.1
+change = -0.2
+
+sizeWeight = 0.1
 
 for i in range(max_iters):
     corr = tryLine(x,y,low,high)
-    score = (0.9 + 0.1*(high - low)) * corr
+    score = (1-sizeWeight + sizeWeight*(high - low)) * corr
     print("score {}".format(score))
 
     low += change
@@ -75,26 +76,38 @@ for i in range(max_iters):
     if score < prevScore + corrStepThresh:
         change = change * -1
 
-    change *= 0.8
+    change *= 0.7
     prevScore = score
 
+    if low < 0 or low > 1:
+        print("resetting low")
+        low = 0
+
 low = low - change
+if low < 0 or low > 1:
+    low = 0
 print()
 
-change = 0.1
+change = 0.2
 for i in range(max_iters):
+    print(high)
     corr = tryLine(x,y,low,high)
-    score = (0.8 + 0.1*(high - low)) * corr
+    score = (1-sizeWeight + sizeWeight*(high - low)) * corr
     print("score {}".format(score))
+
+    high += change
 
     if score < prevScore + corrStepThresh:
         change = change * -1
 
-    change *= 0.8
-    high += change
+    change *= 0.7
     prevScore = score
+    if high < 0 or high > 1:
+        high = 1
 
 high = high - change
+if high < 0 or high > 1:
+    high = 1
 
 plt.plot(x,y)
 line = intercept + slope * x
