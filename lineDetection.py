35cf56fe0b83360fed99e4ae436eea5a6f71bb4f
dziper <dziper@me.com>
2020-11-20 17:26:01 -0800

    actually kinda works but a little too short

--- lineDetection.py
+++ lineDetection.py
@@ -18,7 +18,7 @@ for i in range(split1):
 for i in range(split2 - split1):
     y = np.append(y, y[split1 - 1] + i*1.2)
 for i in range(length - split2):
-    y = np.append(y, y[split2 - 1] + i*0.5)
+    y = np.append(y, y[split2 - 1] + i*0.2)
 
 # add noise
 for i in range(length):
@@ -37,46 +37,67 @@ line = intercept + slope * x
 plt.plot(x, line)
 plt.show()
 
-def tryLine(x,y,low,high):
+def tryLine(x,y,low,high, plot = False):
     totalLength = len(x)
     x_slice = x[int(totalLength*low):int(totalLength*high)]
     y_slice = y[int(totalLength*low):int(totalLength*high)]
     slope, intercept, r_value, p_value, std_err = stats.linregress(x_slice,y_slice)
     corrCoeff = r_value ** 2
     print("low {} high {} corr {}".format(low,high,corrCoeff))
+
+    if plot:
+        plt.plot(x_slice,y_slice)
+        line = intercept + slope * x_slice
+        plt.plot(x_slice, line)
+
     return corrCoeff
+
 
 
 low = 0
 high = 1
 
-corrStepThresh = 0.01
+corrStepThresh = -0.01
 corr = tryLine(x,y,low,high)
-prevCorr = corr
+prevScore = corr
 iters = 0
 max_iters = 10
-low = 0.5
+low = 0
+change = 0.1
 
-while corr < CORR_COEFF_THRESH and iters < max_iters:
-    if corr > prevCorr - corrStepThresh:
-        low = low/2
-    else:
-        low = low+low/2
-
+for i in range(max_iters):
     corr = tryLine(x,y,low,high)
+    score = (0.9 + 0.1*(high - low)) * corr
+    print("score {}".format(score))
 
-    prevCorr = corr
-    iters += 1
+    low += change
 
-CORR_COEFF_THRESH = 0.98
-while corr < CORR_COEFF_THRESH and iters < max_iters:
+    if score < prevScore + corrStepThresh:
+        change = change * -1
+
+    change *= 0.8
+    prevScore = score
+
+low = low - change
+print()
+
+change = 0.1
+for i in range(max_iters):
     corr = tryLine(x,y,low,high)
+    score = (0.8 + 0.1*(high - low)) * corr
+    print("score {}".format(score))
 
-    if corr > prevCorr - corrStepThresh:
-        high = high - high/2
-    else:
-        high = high/2
-    if high == low:
-        print("bad")
-    prevCorr = corr
-    iters += 1
+    if score < prevScore + corrStepThresh:
+        change = change * -1
+
+    change *= 0.8
+    high += change
+    prevScore = score
+
+high = high - change
+
+plt.plot(x,y)
+line = intercept + slope * x
+plt.plot(x, line)
+corr = tryLine(x,y,low,high, plot = True)
+plt.show()
