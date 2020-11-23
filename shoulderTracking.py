41f0d4ac8b82d892da7f12580de69ff1840d310e
dziper <dziper@me.com>
2020-11-23 13:55:14 -0800

    set up for testing in file

--- shoulderTracking.py
+++ shoulderTracking.py
@@ -1,35 +1,16 @@
 #Tutorial: https://www.guidodiepen.nl/2017/02/detecting-and-tracking-a-face-with-python-and-opencv/
 
 #Import the OpenCV library
-import cv2
+import cv2 as cv
 import numpy as np
 from scipy import stats;
 import math
 import lineDetection
 
 #Initialize a face cascade using the frontal face haar cascade provided
-#with the OpenCV2 library
-faceCascade = cv2.CascadeClassifier('CascadeClassifiers/faceCascade.xml')
-colorBounds = ([170, 100, 15], [180, 180, 100])
-
-def calculate_max_contrast_pixel(img_gray, x, y, h, top_values_to_consider=3, search_width = 20):
-    columns = img_gray[int(y):int(y+h), int(x-search_width/2):int(x+search_width/2)];
-
-    column_average = columns.mean(axis=1);
-    gradient = np.gradient(column_average, 3);
-    gradient = np.absolute(gradient); # abs gradient value
-    max_indicies = np.argpartition(gradient, -top_values_to_consider)[-top_values_to_consider:] # indicies of the top 5 values
-    max_values = gradient[max_indicies];
-    if(max_values.sum() < top_values_to_consider): return None; # return none if no large gradient exists - probably no shoulder in the range
-    weighted_indicies = (max_indicies * max_values);
-    weighted_average_index = weighted_indicies.sum() / max_values.sum();
-
-    if math.isnan(weighted_average_index):
-        return None
-
-    index = int(weighted_average_index);
-    index = y + index;
-    return index;
+#with the Opencv library
+faceCascade = cv.CascadeClassifier('CascadeClassifiers/faceCascade.xml')
+colorBounds = ([170, 100, 15], [185, 180, 190])
 
 def highestWhite(img_gray, x, minY = 0):
     x = int(x)
@@ -88,7 +69,7 @@ def draw_line_onto_image(img, line, color="GREEN"):
     if(color=="GREEN"): color = (0, 255, 0);
     if(color=="BLUE"): color = (255, 0, 0);
     if(color=="RED"): color = (0, 0, 255);
-    cv2.line(img, beginning, end, color, 1)
+    cv.line(img, beginning, end, color, 1)
     return img;
 
 def plotPoints(img, pointList, color = (0,0,255)):
@@ -97,21 +78,15 @@ def plotPoints(img, pointList, color = (0,0,255)):
         y = int(point[1])
         img[y,x] = color
 
-
 def detectShoulders(img_gray, mask):
     #Now use the haar cascade detector to find all faces in the image
     faces = faceCascade.detectMultiScale(img_gray, 1.3, 5)
 
-    #For now, we are only interested in the 'largest' face, and we
-    #determine this based on the largest area of the found
-    #rectangle. First initialize the required variables to 0
     maxArea = 0
     x = 0
     y = 0
     w = 0
     h = 0
-
-
     #Loop over all faces and check if the area for this face is
     #the largest so far
     bigFace = None
@@ -125,20 +100,13 @@ def detectShoulders(img_gray, mask):
             h = _h
             maxArea = w*h
 
-        #If one or more faces are found, draw a rectangle around the
-        #largest face present in the picture
         if maxArea > largestArea:
             largestArea = maxArea
             bigFace = face
 
-
     if largestArea > 0:
         # print("new frame")
-
-        # cv2.rectangle(img_gray,  (bigFace[0]-10, bigFace[1]-20),(bigFace[0] + bigFace[2]+10 , bigFace[1] + bigFace[3]+20),rectangleColor,2)
-
-
-
+        # cv.rectangle(img_gray,  (bigFace[0]-10, bigFace[1]-20),(bigFace[0] + bigFace[2]+10 , bigFace[1] + bigFace[3]+20),rectangleColor,2)
         lower = np.array(colorBounds[0], dtype="uint8")
         upper = np.array(colorBounds[1], dtype="uint8")
 
@@ -148,3 +116,25 @@ def detectShoulders(img_gray, mask):
         return right_line, right_slope, left_line, left_slope
 
     return None
+
+
+stream = cv.VideoCapture(0)
+
+name = "frames"
+cv.namedWindow(name)
+
+while True:
+    grabbed, frame = stream.read()
+    key = cv.waitKey(1) & 0xFF
+
+    lower = np.array(colorBounds[0], dtype="uint8")
+    upper = np.array(colorBounds[1], dtype="uint8")
+
+    hsvFrame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    mask = cv.inRange(hsvFrame, lower, upper)
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    cv.imshow(name, mask)
+
+cv.destroyAllWindows()
