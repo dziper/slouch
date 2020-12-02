b9569fab1afe24bea5ea8952c4cd82697b18ac15
dziper <dziper@me.com>
2020-12-01 17:22:15 -0800

    tune classify()

--- dataClassifier.py
+++ dataClassifier.py
@@ -5,43 +5,54 @@ class DataClassifier:
     def __init__(self,frameData, calibratedData):
         if len(frameData) != 8 or len(calibratedData) != 8:
             print("classifier is NONE")
-        self.right_eye = (frameData[0], calibratedData[0])
-        self.left_eye = (frameData[1], calibratedData[1])
-        self.right_beginning = (frameData[2], calibratedData[2])
-        self.right_end = (frameData[3], calibratedData[3])
-        self.right_slope = (frameData[4], calibratedData[4])
-        self.left_beginning = (frameData[5], calibratedData[5])
-        self.left_end = (frameData[6], calibratedData[6])
-        self.left_slope = (frameData[7], calibratedData[7])
+        self.right_eye = [frameData[0], calibratedData[0]]
+        self.left_eye = [frameData[1], calibratedData[1]]
+        self.right_beginning = [frameData[2], calibratedData[2]]
+        self.right_end = [frameData[3], calibratedData[3]]
+        self.right_slope = [frameData[4], calibratedData[4]]
+        self.left_beginning = [frameData[5], calibratedData[5]]
+        self.left_end = [frameData[6], calibratedData[6]]
+        self.left_slope = [frameData[7], calibratedData[7]]
 
     #update data
     def newData(self, data, classify = False):
-        if len(frameData) != 8 or len(calibratedData) != 8:
+        if len(data) != 8:
             return False
 
-        self.right_eye[classify] = frameData[0]
-        self.left_eye[classify] = frameData[1]
-        self.right_beginning[classify] = frameData[2]
-        self.right_end[classify] = frameData[3]
-        self.right_slope[classify] = frameData[4]
-        self.left_beginning[classify] = frameData[5]
-        self.left_end[classify] = frameData[6]
-        self.left_slope[classify] = frameData[7]
+        self.right_eye[classify] = data[0]
+        self.left_eye[classify] = data[1]
+        self.right_beginning[classify] = data[2]
+        self.right_end[classify] = data[3]
+        self.right_slope[classify] = data[4]
+        self.left_beginning[classify] = data[5]
+        self.left_end[classify] = data[6]
+        self.left_slope[classify] = data[7]
 
         return True
 
     def classify(self):
         weightedSum = 0
 
-        angleWeight = 1
-        weightedSum += np.abs(self.getAngleDifference()/180 * angleWeight)
+        angleWeight = 20
+        angleDiffVal = np.abs(self.getAngleDifference()/180 * angleWeight)
 
-        ratioDifference = self.getEyeShoulderRatio(classify = True) - self.getEyeShoulderRatio()
-        invRatioDifference = 1/self.getEyeShoulderRatio(classify = True) - 1/self.getEyeShoulderRatio()
-        ratioWeight = 1
-        weightedSum += np.abs(ratioDifference+invRatioDifference) * ratioWeight
+        ratioDifference = self.getEyeShoulderRatio(classify = True)/self.getEyeShoulderRatio()
+        invRatioDifference = 1/ratioDifference
+        ratioDifference = max(ratioDifference,invRatioDifference) - 1
+        ratioWeight = 0.1
+        ratioVal = np.abs(ratioDifference) * ratioWeight
 
-        return sigmoid(weightedSum)
+        print("angle")
+        print(angleDiffVal)
+        print("ratio")
+        print(ratioDifference)
+
+        weightedSum += angleDiffVal + ratioVal
+
+        print("sum")
+        print(weightedSum)
+
+        return DataClassifier.sigmoid(weightedSum)
 
     #returns angle difference between calib and frame angle in degrees
     def getAngleDifference(self):
@@ -62,5 +73,6 @@ class DataClassifier:
         shoulderWidth = self.right_beginning[classify][0] - self.left_beginning[classify][0]
         return eyeWidth/shoulderWidth
 
+    @staticmethod
     def sigmoid(num):
         return 1/(1 + np.exp(-num))
